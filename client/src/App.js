d77569c98ef0e4b5432c80466a998da915024677
aniket <aniketnagrale83@gmail.com>
2023-03-05 11:33:14 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,95 @@
+import { useEffect, useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+const api_base = 'http://localhost:3001';
+
+function App() {
+	const [todos, setTodos] = useState([]);
+	const [popupActive, setPopupActive] = useState(false);
+	const [newTodo, setNewTodo] = useState("");
+
+	useEffect(() => {
+		GetTodos();
+	}, []);
+
+	const GetTodos = () => {
+		fetch(api_base + '/todos')
+			.then(res => res.json())
+			.then(data => setTodos(data))
+			.catch((err) => console.error("Error: ", err));
+	}
+
+	const completeTodo = async id => {
+		const data = await fetch(api_base + '/todo/complete/' + id).then(res => res.json());
+
+		setTodos(todos => todos.map(todo => {
+			if (todo._id === data._id) {
+				todo.complete = data.complete;
+			}
+
+			return todo;
+		}));
+		
+	}
+
+	const addTodo = async (e) => {
+		e.preventDefault();
+		const data = await fetch(api_base + "/todo/new", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json" 
+			},
+			body: JSON.stringify({
+				text: newTodo
+			})
+		}).then(res => res.json());
+
+		setTodos([...todos, data]);
+
+		setPopupActive(false);
+		setNewTodo("");
+	}
+
+	const deleteTodo = async id => {
+		const data = await fetch(api_base + '/todo/delete/' + id, { method: "DELETE" }).then(res => res.json());
+
+		setTodos(todos => todos.filter(todo => todo._id !== data.result._id));
+	}
+
+	return (
+		<div className="App">
+			<h1>Hello Developers 🚀</h1>
+			<h4>Your tasks</h4>
+
+			<div className="todos">
+				{todos.length > 0 ? todos.map(todo => (
+					<div className={
+						"todo" + (todo.complete ? " is-complete" : "")
+					} key={todo._id} onClick={() => completeTodo(todo._id)}>
+						<div className="checkbox"></div>
+
+						<div className="text">{todo.text}</div>
+
+						<div className="delete-todo-d" onClick={() => deleteTodo(todo._id)}><FaTrashAlt /></div>
+					</div>
+				)) : (
+					<p style={{color: "#778899" , fontSize:"25px",textAlign:"center"}}>You currently have no tasks 😴 </p>
+				)}
+			</div>
+
+			<div className="addPopup" onClick={() => setPopupActive(true)}>+</div>
+
+			{popupActive ? (
+				<form onSubmit={addTodo} className="popup">
+					<div className="closePopup" onClick={() => setPopupActive(false)}>X</div>
+					<div className="content">
+						<h3>Add Task</h3>
+						<input type="text" className="add-todo-input" onChange={e => setNewTodo(e.target.value)} value={newTodo} />
+						<button disabled={!newTodo} className="button" >Create Task</button>
+					</div>
+				</form>
+			) : ''}
+		</div>
+	);
+}
+
+export default App;
